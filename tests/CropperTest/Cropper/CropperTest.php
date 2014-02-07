5ec42136d085fdd4162ecca82c467fe9f7e6bdb0
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2014-02-08 00:05:07 +0100

    updated tests

--- tests/CropperTest/Cropper/CropperTest.php
+++ tests/CropperTest/Cropper/CropperTest.php
@@ -3,11 +3,37 @@ namespace CropperTest\Cropper;
 
 use PHPUnit_Framework_TestCase;
 
+use CropperTest\Bootstrap;
+
+use Cropper\Cropper\Cropper;
+
 class CropperTest 
 	extends PHPUnit_Framework_TestCase
 {
+	protected $obj;
+	
+	protected $testImagePath;
+	
+	protected $testImageName = 'gatti.jpg';
+	
 	protected function setUp()
 	{
-		
+        $serviceManager = Bootstrap::getServiceManager();
+	    $thumbnailerService = $serviceManager->get('Thumbnailer');
+		$this->testImagePath = dirname(__DIR__) . DIRECTORY_SEPARATOR . '..' 
+		    . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR;
+	    $this->obj = new Cropper($thumbnailerService);
+		$this->obj->open($this->testImagePath . $this->testImageName);
+	}
+	
+	/**
+	 * @covers \Cropper\Cropper\Cropper::__construct()
+	 * @covers \Cropper\Cropper\Cropper::getType()
+	 * @covers \Cropper\Cropper\Cropper::setType()
+	 */
+	public function testGetDefaultTypeInConstruct() 
+	{
+		$type = $this->obj->getType();
+		$this->assertEquals(Cropper::CROPPER_TYPE_CENTER, $type);
 	}
 }
